39ee579dcb176a20cd51c1243ab4d4bc2a9f4527
imranquadri3107 <imranquadri77@gmail.com>
2019-02-04 00:01:32 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Post from './components/Post';
+import Postform from './components/Postform';
+//import { applyMiddleware } from '../../../../AppData/Local/Microsoft/TypeScript/3.2/node_modules/redux';
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store = {store}>
+        <div className="App">
+        
+            <img src={logo} className="App-logo" alt="logo" />
+
+          <Postform/>
+          <hr/>
+          <Post/>
+        </div>
+      </Provider>
     );
   }
 }
