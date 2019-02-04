3287af095ca0cf63436b677b37fc25075f14774d
imranquadri3107 <imranquadri77@gmail.com>
2019-02-04 00:12:56 -0800

    updated

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,8 @@
 import React, { Component } from 'react'
 
  class Post extends Component {
-     constructor(props){
-         super(props);
-         this.state = {
-             posts: []
-         }
-     }
 
-
-
-
-
-     componentDidMount(){
-         fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res=> res.json())
-            .then(data => this.setState({ posts: data}));
-     }
+     
   render() {
       const postItems = this.state.posts.map(post=>(
           <div key = {post.id}>
